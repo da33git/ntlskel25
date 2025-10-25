6e4246d5ed0762119c92c1658857333e4fb97eac
da33git <https://da33git@github.com>
2025-10-24 23:20:49 -0400

    initial

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',"./node_modules/@brainandbones/skeleton/**/*.{html,js,svelte,ts}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("@brainandbones/skeleton/tailwind/theme.cjs")],
+}
